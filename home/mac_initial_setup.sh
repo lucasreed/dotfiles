6f0db49d3b188adf09c02b7d101941f1c8b5f44e
Luke Reed <luke@lreed.net>
2020-09-21 10:00:35 -0400

    adding initial setup script for mac

---
+++ home/mac_initial_setup.sh
@@ -0,0 +1,14 @@
+if ! command -v brew &> /dev/null
+then
+    echo 'Homebrew is not installed, run: /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"'
+    exit 1
+fi
+
+brew bundle
+
+mkdir -p ~/.vim/bundle
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
+git clone https://github.com/preservim/nerdtree.git ~/.vim/bundle/nerdtree
+git clone https://github.com/dracula/vim.git ~/.vim/bundle/dracula
